8e544d8103ed046daea920ed0a47e3afc0dcfa27
Job Klarenbeek <jjklarenbeek@hotmail.com>
2023-12-22 17:43:49 +0100

    rework: finishing touches & updates

--- src/app/artikels/4/page.tsx
+++ src/app/artikels/4/page.tsx
@@ -1,28 +1,22 @@
-"use client";
-
-import Script from "next/script";
 import config from "@/lib/Config.json";
 
 export default function Home() {
     var ConfigId = parseFloat(location.pathname.slice(10));
 
     return (
-        <>
-            <Script src="sectionManager.js" />
-            <section id="artikel" className="artikel-container">
-                <img src={config.NewsItem[ConfigId - 1].image} />
-                <div className="artikel-info">
-                    <div>
-                        <p>{config.NewsItem[ConfigId - 1].author}</p>
-                        {"・"}
-                        <p>{config.NewsItem[ConfigId - 1].datum}</p>
-                    </div>
-                    <h1>{config.NewsItem[ConfigId - 1].title}</h1>
-                    {config.NewsItem[ConfigId - 1].text.map((data, key) => (
-                        <p key={key}>{data}</p>
-                    ))}
+        <section id="artikel" className="artikel-container">
+            <img src={config.NewsItem[ConfigId - 1].image} />
+            <div className="artikel-info">
+                <div>
+                    <p>{config.NewsItem[ConfigId - 1].author}</p>
+                    {"・"}
+                    <p>{config.NewsItem[ConfigId - 1].datum}</p>
                 </div>
-            </section>
-        </>
+                <h1>{config.NewsItem[ConfigId - 1].title}</h1>
+                {config.NewsItem[ConfigId - 1].text.map((data, key) => (
+                    <p key={key}>{data}</p>
+                ))}
+            </div>
+        </section>
     );
 }
